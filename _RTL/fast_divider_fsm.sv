7f25941538244eb2b4357a16334148ea1bf6b0ce
LCP2K4 <142216963+LCP2K4@users.noreply.github.com>
2026-03-03 00:45:09 -0800

    Add files via upload

--- _RTL/fast_divider_fsm.sv
+++ _RTL/fast_divider_fsm.sv
@@ -21,10 +21,11 @@
 
 
 module fast_divider_fsm(
-    input start,clk,rs,
-    output reg r1_ld,r2_ld,r3_ld,r4_ld
+    input clk,rs,start,
+    output reg r1_ld,r2_ld,r3_ld,r4_ld,r5_ld,
+    output reg done
     );
-     typedef enum {IDLE, START, R1,R2,R3,R4,DONE} state_e;
+    typedef enum {IDLE, R1,R2,R3,R4,R5,DONE_S} state_e;
     state_e state, next_state;
     always @(posedge clk or posedge rs) begin
         if(rs) state <= IDLE;
@@ -32,4 +33,40 @@ module fast_divider_fsm(
     end
     always @(*) begin
         case(state)
+            IDLE: begin
+                {r5_ld,r4_ld,r3_ld,r2_ld,r1_ld} = 5'b0;
+                done = 1'b0;
+                if(start)
+                    next_state = R1;
+                else
+                    next_state = IDLE;
+            end
+            R1: begin
+                {r5_ld,r4_ld,r3_ld,r2_ld,r1_ld} = 5'b00001;
+                next_state = R2;
+            end
+            R2: begin
+                {r5_ld,r4_ld,r3_ld,r2_ld,r1_ld} = 5'b00010;
+                next_state = R3;
+            end
+            R3: begin
+                {r5_ld,r4_ld,r3_ld,r2_ld,r1_ld} = 5'b00100;
+                next_state = R4;
+            end
+            R4: begin
+                {r5_ld,r4_ld,r3_ld,r2_ld,r1_ld} = 5'b01000;
+                next_state = R5;
+            end
+            R5: begin
+                {r5_ld,r4_ld,r3_ld,r2_ld,r1_ld} = 5'b10000;
+                next_state = DONE_S;
+            end
+            DONE_S: begin
+                {r5_ld,r4_ld,r3_ld,r2_ld,r1_ld} = 5'b00000;
+                done = 1'b1;
+                next_state = IDLE;
+            end  
+            default: next_state = IDLE;
+        endcase
+     end
 endmodule
