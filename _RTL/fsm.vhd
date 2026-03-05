3844fc6b6c5e6b6aad6bb78b219aa2ddeadbe2f5
LCP2K4 <142216963+LCP2K4@users.noreply.github.com>
2026-03-05 01:31:59 -0800

    Add new state S10 to the controller FSM

--- _RTL/fsm.vhd
+++ _RTL/fsm.vhd
@@ -1,5 +1,35 @@
+----------------------------------------------------------------------------------
+-- Company: 
+-- Engineer: 
+-- 
+-- Create Date: 11/11/2025 06:14:21 PM
+-- Design Name: 
+-- Module Name: controller - Behavioral
+-- Project Name: 
+-- Target Devices: 
+-- Tool Versions: 
+-- Description: 
+-- 
+-- Dependencies: 
+-- 
+-- Revision:
+-- Revision 0.01 - File Created
+-- Additional Comments:
+-- 
+----------------------------------------------------------------------------------
+
+
 library IEEE;
 use IEEE.STD_LOGIC_1164.ALL;
+
+-- Uncomment the following library declaration if using
+-- arithmetic functions with Signed or Unsigned values
+--use IEEE.NUMERIC_STD.ALL;
+
+-- Uncomment the following library declaration if instantiating
+-- any Xilinx leaf cells in this code.
+--library UNISIM;
+--use UNISIM.VComponents.all;
 
 entity controller is
     Port ( clk : in STD_LOGIC;
@@ -8,7 +38,10 @@ entity controller is
            gt_4 : in STD_LOGIC;
            div_flag : in STD_LOGIC;
            start: in STD_LOGIC;
+           mode_sl : in STD_LOGIC;
+           mode : out STD_LOGIC;
            preset : out STD_LOGIC := '0';
+           input_ld : out STD_LOGIC := '0';
            x_sel : out STD_LOGIC := '0';
            x_ld : out STD_LOGIC := '0';
            out_ld : out STD_LOGIC := '0';
@@ -19,134 +52,167 @@ entity controller is
 end controller;
 
 architecture Behavioral of controller is
-    type state_type is (S0,S1,S2,S3,S4,S5,S6,S7,S8,S9 );
+    type state_type is (S0,S1,S2,S3,S4,S5,S6,S7,S8,S9,S10 );
     signal state, next_state : state_type;
+    signal mode_reg : STD_LOGIC;
 begin
 
     process(clk,reset) begin
         if reset = '1' then
             state <= S0;
+            mode_reg <= '0';
          elsif rising_edge(clk) then
-            state <= next_state;
+            if start = '1' and state = S0 then 
+                mode_reg <= mode_sl;
+                mode <= mode_sl;
+                state <= next_state;
+            else    state <= next_state;
+            end if;
          end if;
     end process;
     
-    process(state,div_flag,n_flag,gt_4,start)
+    process(state,div_flag,n_flag,gt_4,start,mode_sl,mode_reg)
     begin
     case state is
         when S0 => 
             preset <= '1';
             x_sel  <= '0';
             x_ld   <= '0';
+            input_ld <= '0';
             out_ld <= '0';
             cnt_ld <= '0';
             div_ld <= '0';
             temp_ld <= '0';
             done   <= '0';
-            if start = '1' then next_state <= S1;
+            if start = '1' then
+                next_state <= S1;
             else next_state <= S0;
             end if;
           
          when S1 =>
             preset <= '0';
             out_ld <= '0';
-            cnt_ld <= '1';
+            cnt_ld <= '0';
             div_ld <= '0';
             temp_ld <= '0';
             x_sel <= '0';
             x_ld <= '1';
+            input_ld <= '1';
             done   <= '0';
             next_state <= S2;
-            
+          
           when S2 =>
+            preset <= '0';
+            out_ld <= '0';
+            cnt_ld <= '0';
+            div_ld <= '0';
+            temp_ld <= '0';
+            x_sel <= '0';
+            x_ld <= '1';
+            input_ld <= '0';
+            done   <= '0';
+            if mode_reg = '1' then next_state <= S3;
+            else next_state <= S5;
+            end if; 
+          when S3 =>
             preset <= '0';
             x_sel  <= '0';
             x_ld   <= '0';
+            input_ld <= '0';
             out_ld <= '0';
             cnt_ld <= '0';
             div_ld <= '0';
             temp_ld <= '0';
             done   <= '0';
-            if n_flag = '1' then
-                next_state <= S8;
+            if n_flag = '0' then
+                next_state <= S4;
             else
-                next_state <= S3;
+                next_state <= S9;
             end if;
             
-          when S3 =>
+          when S4 =>
             preset <= '0';
             x_sel  <= '0';
             x_ld   <= '0';
+            input_ld <= '0';
             out_ld <= '0';
             cnt_ld <= '0';
             div_ld <= '0';
             temp_ld <= '0';
             done   <= '0';
             if gt_4 = '1' then
-                next_state <= S8;
+                next_state <= S9;
             else
-                next_state <= S4;
+                next_state <= S5;
             end if;
-         when S4 =>
+         when S5 =>
             preset <= '0';
             x_sel  <= '0';
             x_ld   <= '0';
+            input_ld <= '0';
             out_ld <= '0';
             cnt_ld <= '0';
             div_ld <= '1';
             temp_ld <= '0';
             done   <= '0';
-            next_state <= S5;
-          when S5 =>
+            next_state <= S6;
+          when S6 =>
             preset <= '0';
             x_sel  <= '0';
             x_ld   <= '0';
+            input_ld <= '0';
             out_ld <= '0';
             cnt_ld <= '0';
             div_ld <= '0';
             temp_ld <= '0';
             done   <= '0';
             if div_flag = '1' then
-                next_state <= S6;
+                next_state <= S7;
             else
-                next_state <= S5;
+                next_state <= S6;
             end if;
-          when S6 =>
+          when S7 =>
             preset <= '0';
             cnt_ld <= '0';
             x_sel  <= '0';
             x_ld   <= '0';
+            input_ld <= '0';
             out_ld <= '0';
             div_ld <= '0';
             temp_ld <= '1';
             done   <= '0';
-            next_state <= S7;
-          when S7 =>
+            next_state <= S8;
+          when S8 =>
             preset <= '0';
             cnt_ld <= '1';
             x_sel  <= '1';
             x_ld   <= '1';
+            input_ld <= '0';
             out_ld <= '0';
             div_ld <= '0';
             temp_ld <= '0';
             done   <= '0';
-            next_state <= S3;  
-          when S8 =>
-            preset <= '0';
-            cnt_ld <= '0';
-            x_sel  <= '0';
-            x_ld   <= '0';
-            out_ld <= '1';
-            div_ld <= '0';
-            temp_ld <= '0';
-            done   <= '0';
-            next_state <= S9;
-            
+            if mode_reg = '1' then next_state <= S4;
+            else next_state <=  S9; 
+            end if;
           when S9 =>
             preset <= '0';
             cnt_ld <= '0';
             x_sel  <= '0';
             x_ld   <= '0';
+            input_ld <= '0';
+            out_ld <= '1';
+            div_ld <= '0';
+            temp_ld <= '0';
+            done   <= '0';
+            next_state <= S10;
+            
+          when S10 =>
+            preset <= '0';
+            cnt_ld <= '0';
+            x_sel  <= '0';
+            x_ld   <= '0';
+            input_ld <= '0';
             out_ld <= '0';
             div_ld <= '0';
             temp_ld <= '0';
