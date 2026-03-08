c7990f95c7fba76b275fc2eb53733a45045bb9cc
LCP2K4 <142216963+LCP2K4@users.noreply.github.com>
2026-03-08 23:52:38 +0700

    Extend Decoder module to handle 33-bit outputs

--- _RTL/Decoder.sv
+++ _RTL/Decoder.sv
@@ -23,17 +23,17 @@
 module Decoder(
     input [31 : 0] X,
     input [2 : 0] De_Sel,
-    output [31 : 0] De_Out
+    output [32 : 0] De_Out
     );
-    reg [31 : 0] En_Temp;
+    reg [32 : 0] En_Temp;
     always @(*) begin
         case(De_Sel)
-            3'b000 : En_Temp = 32'b0;
-            3'b001 : En_Temp = X;
-            3'b010 : En_Temp = X << 1;
-            3'b110 : En_Temp = (~(X << 1)) + 1;
-            3'b101 : En_Temp = (~X) + 1;
-            default : En_Temp = 32'b0;
+            3'b000 : En_Temp = 33'b0;
+            3'b001 : En_Temp = {X[31],X};
+            3'b010 : En_Temp = {X[31],X} << 1;
+            3'b110 : En_Temp = (~({X[31],X} << 1)) + 1;
+            3'b101 : En_Temp = (~{X[31],X}) + 1;
+            default : En_Temp = 33'b0;
          endcase
     end
     assign De_Out = En_Temp; 
