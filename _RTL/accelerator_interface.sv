3edb008f0fc03ea02ba70d6142a25a5f206f239e
LCP2K4 <142216963+LCP2K4@users.noreply.github.com>
2026-03-08 23:59:57 +0700

    Update accelerator_interface.sv

--- _RTL/accelerator_interface.sv
+++ _RTL/accelerator_interface.sv
@@ -21,27 +21,30 @@
 
 
 module accelerator_interface #(parameter N = 32)(
-    input [N-1:0] X[8 : 0],
+    input [N-1:0] X[7 : 0],
     input start, clk, rs,
     output [N-1:0] Y
     );
     
     genvar i;
-    wire done [8 : 0];
-    wire [N - 1 : 0] Pe_out[8 : 0];
-    wire [N : 0] s1 [2 : 0];
-    wire c1 [2 : 0];
-    wire [N - 1 : 0] s2,temp;
-    wire c2;
+    wire done [7 : 0];
+    wire [N - 1 : 0] Pe_out[7 : 0];
+    wire [N - 1 : 0] s1 [3 : 0];
+    wire [N - 1 : 0] s2[1 : 0];
+    wire [N - 1 : 0] s3;
+    wire [N - 1 : 0] temp;
 generate
-    for(i = 0; i < 9; i ++) begin : pe_proc
+    for(i = 0; i < 8; i ++) begin : pe_proc
         PE_Interface pe(X[i],clk,rs,start, Pe_out[i],done[i]);
     end
-    for(i = 0; i < 3; i ++) begin : lv1
-        CSA ca1(Pe_out[3*i],Pe_out[3*i + 1],Pe_out[3*i + 2],s1[i],c1[i]);
+    for(i = 0; i < 4; i ++) begin : lv1
+        assign s1[i] =  Pe_out[2*i] + Pe_out[2*i + 1];
+    end
+    for(i = 0; i < 2; i ++) begin : lv2
+        assign s2[i] =  s1[2*i] + s1[2*i + 1];
     end
 endgenerate
-    CSA ca2({c1[0],s1[0]},{c1[1],s1[1]},{c1[2],s1[2]},s2,c2);    
-    register re1({c2,s2},clk,rs,done[0],temp);
+    assign s3 = s2[0] + s2[1];   
+    Accumulator Acc(s3,clk,rs,done[0],temp);
     assign Y = temp;
 endmodule
